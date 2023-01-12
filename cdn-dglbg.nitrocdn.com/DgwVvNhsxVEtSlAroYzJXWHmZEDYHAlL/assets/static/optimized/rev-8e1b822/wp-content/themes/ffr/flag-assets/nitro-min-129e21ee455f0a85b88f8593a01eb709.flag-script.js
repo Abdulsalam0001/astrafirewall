d26356f93bd5553329e6ec29f6bb255bc099298c
chris chase <dreshaw2012@gmail.com>
2023-01-12 04:40:48 -0800

    skfjdos

--- cdn-dglbg.nitrocdn.com/DgwVvNhsxVEtSlAroYzJXWHmZEDYHAlL/assets/static/optimized/rev-8e1b822/wp-content/themes/ffr/flag-assets/nitro-min-129e21ee455f0a85b88f8593a01eb709.flag-script.js
+++
@@ -1,7 +0,0 @@
-$(".flag-contact").intlTelInput({
-  utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/8.4.6/js/utils.js"
-});
-
-$(".flag-contact_popup").intlTelInput({
-  utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/8.4.6/js/utils.js"
-});
